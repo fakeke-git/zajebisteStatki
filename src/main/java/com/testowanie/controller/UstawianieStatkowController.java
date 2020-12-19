6eef56aa27aa846ccb6c148a80af529910e775cf
Kubuś Puchatek <kub4.w@wp.pl>
2020-12-19 23:30:51 +0100

    Zacznij ustawianie statków
    Zmieniłem w widoku fxml - pod liczbą statków do umiejscowienia wyświetlony jest symboliczny statek danej wielkości. Można kliknąć na HBoxa w którym umiejcowione są te buttony i przeciągnąć myszką na plansze - podświetli się miejsce do umieszczenia statku. Do zrobienia na pewno jeszcze opcje zmieniania pion/poziom, blokowanie opcji stawiania statków obok siebie, no i same stawianie statków na akcji clicku.

--- src/main/java/com/testowanie/controller/UstawianieStatkowController.java
+++ src/main/java/com/testowanie/controller/UstawianieStatkowController.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.RowConstraints;
 import javafx.stage.Stage;
 
@@ -33,11 +34,19 @@ public class UstawianieStatkowController {
     private Label liczbaJeden;
 
     @FXML
+    private HBox czteroHbox;
+    
+    @FXML
     private GridPane plansza1;
+    
+    private Button tablicaPrzyciskow[][];
+    private boolean czyUstawianie;
+    private int iloMasztowy;
 
     public void initialize() {
         ustawienia = (Ustawienia) Main.primaryStage.getUserData();
 
+        tablicaPrzyciskow = new Button[ustawienia.getRozmiarPlansz()][ustawienia.getRozmiarPlansz()];
         dodajP(plansza1, ustawienia.getRozmiarPlansz());
         asd(plansza1, ustawienia.getRozmiarPlansz());
 
@@ -66,17 +75,50 @@ public class UstawianieStatkowController {
                 double pv = width / rozmiar ;
                 double ph = height / rozmiar;
                 Button b = new Button();
+                tablicaPrzyciskow[i][j] = b;
                 b.setStyle("-fx-background-color: aqua");
                 b.setPrefWidth(pv);
                 b.setPrefHeight(ph);
                 p.add(b, j, i);
 
-                b.setUserData(false);
+                b.setUserData(new ButtonProperties(i, j, false));
 
-                b.setOnAction((e) -> {
-                    b.setUserData(!((Boolean) b.getUserData()));
-                    if ((Boolean) b.getUserData()) b.setStyle("-fx-background-color: coral");
-                    else b.setStyle("-fx-background-color: aqua");
+//                b.setOnAction((e) -> {
+//                    b.setUserData(!((Boolean) b.getUserData()));
+//                    if ((Boolean) b.getUserData()) b.setStyle("-fx-background-color: coral");
+//                    else b.setStyle("-fx-background-color: aqua");
+//                });
+                
+                b.setOnMouseEntered(e -> {
+                	if(czyUstawianie) {
+                		ButtonProperties buttonProperties = (ButtonProperties)((Button)e.getSource()).getUserData();
+                		int x = buttonProperties.getX();
+                		int y = buttonProperties.getY();
+
+                		if(!(iloMasztowy + y > ustawienia.getRozmiarPlansz())) { 
+                		for(int k = iloMasztowy; k > 0; k--) {
+                			
+                			tablicaPrzyciskow[x][y].setStyle("-fx-background-color: coral");
+                			y++;
+                			
+                		}
+                		
+                	}}
+                });
+                
+                b.setOnMouseExited(e -> {
+                	if(czyUstawianie) {
+                		ButtonProperties buttonProperties = (ButtonProperties)((Button)e.getSource()).getUserData();
+                		int x = buttonProperties.getX();
+                		int y = buttonProperties.getY();
+                		for(int k = iloMasztowy; k > 0; k--) {
+                			if(!(k + y > ustawienia.getRozmiarPlansz())) {
+                			tablicaPrzyciskow[x][y].setStyle("-fx-background-color: aqua");
+                			y++;
+                			}
+                		}
+                		
+                	}
                 });
             }
         }
@@ -97,4 +139,50 @@ public class UstawianieStatkowController {
 
         stage.setScene(gierka);
     }
+    
+    public void ustawStatek(Event e) {
+    	czyUstawianie = true;
+    	iloMasztowy = ((HBox) e.getSource()).getChildren().size();
+    	
+    }
+    
+    private class ButtonProperties {
+    	private int x;
+    	private int y;
+    	private boolean zajety;
+    	
+		public ButtonProperties(int x, int y, boolean zajety) {
+			this.x = x;
+			this.y = y;
+			this.zajety = zajety;
+		}
+
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
+
+		public boolean isZajety() {
+			return zajety;
+		}
+
+		public void setZajety(boolean zajety) {
+			this.zajety = zajety;
+		}
+    	
+    
+    }
+    
+
 }
